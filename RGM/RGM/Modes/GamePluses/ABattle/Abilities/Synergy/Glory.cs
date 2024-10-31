5c8703426008d0fc98f1674fb03981d98b09ed5e
GoldenPig1205 <pkj7354@gmail.com>
2024-10-31 18:04:02 +0900

    워업ㅣ리펙토링 진행 중 (8/10)

---
+++ RGM/RGM/Modes/GamePluses/ABattle/Abilities/Synergy/Glory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs.Player;
+using InventorySystem.Items.Usables.Scp330;
+using MapEditorReborn.API.Features.Objects;
+using MapEditorReborn.API.Features.Serializable;
+using MapEditorReborn.API.Features;
+using MEC;
+using RGM.API.Features;
+using UnityEngine;
+
+namespace RGM.Modes.Abilities.Synergy;
+
+[RequiresAbility([AbilityType.LEGEND_FLASHLIGHT, AbilityType.NORMAL_TORCH])]
+[Ability("광휘", "<플래시라이트, 횃불> 당신을 쳐다보는 눈은 멀어버릴 것입니다.", AbilityCategory.Synergy, AbilityType.SYNERGY_GLORY)]
+public class Glory : Ability
+{
+    CoroutineHandle _radiation;
+
+    public override void OnEnabled()
+    {
+        _radiation = Timing.RunCoroutine(Radiation());
+    }
+
+    public override void OnDisabled()
+    {
+        Timing.KillCoroutines(_radiation);
+    }
+
+    public IEnumerator<float> Radiation()
+    {
+        LightSourceSerializable LightSource = new LightSourceSerializable("#FFD700", 100, 20, true);
+        LightSourceObject Light = ObjectSpawner.SpawnLightSource(LightSource, Vector3.zero);
+
+        while (true)
+        {
+            if (Tools.TryGetLookPlayer(Owner, 45f, out Player target))
+            {
+                if (Owner != target && Owner.LeadingTeam != target.LeadingTeam)
+                {
+                    Light.Position = target.Position;
+
+                    Hitmarker.SendHitmarkerDirectly(target.ReferenceHub, 0.8f);
+                    Owner.EnableEffect(EffectType.Flashed, 1, 1f);
+                }
+            }
+
+            yield return Timing.WaitForSeconds(0.1f);
+        }
+    }
+}
---
+++ RGM/RGM/Modes/GamePluses/ABattle/Abilities/Synergy/SurvivalExpert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs.Player;
+using InventorySystem.Items.Usables.Scp330;
+using MEC;
+using RGM.API.Features;
+using UnityEngine;
+
+namespace RGM.Modes.Abilities.Synergy;
+
+[RequiresAbility([AbilityType.NORMAL_INSURANCE, AbilityType.NORMAL_DOPAMINE, AbilityType.EPIC_SURVIVOR])]
+[Ability("생존 전문가", "<보험, 도파민, 구사일생> 즉시 현재 체력의 500%에 해당하는 최대 체력을 얻습니다.", AbilityCategory.Synergy, AbilityType.SYNERGY_SURVIVALEXPERT)]
+public class SurvivalExpert : Ability
+{
+    public override void OnEnabled()
+    {
+        Owner.Health = Owner.Health * 5;
+
+        if (Owner.MaxHealth < Owner.Health)
+            Owner.MaxHealth = Owner.Health;
+    }
+
+    public override void OnDisabled()
+    {
+    }
+}
