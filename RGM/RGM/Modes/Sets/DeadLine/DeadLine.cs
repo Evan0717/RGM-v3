4d4eeb441d302949d13cfd3555f9b5cc95a8352d
GoldenPig1205 <pkj7354@gmail.com>
2024-12-16 22:22:19 +0900

    데드라인!!!!!!!

--- RGM/RGM/Modes/Sets/DeadLine/DeadLine.cs
+++ RGM/RGM/Modes/Sets/DeadLine/DeadLine.cs
@@ -132,7 +132,7 @@ namespace RGM.Modes
                 {
                     Round.IsLocked = false;
 
-                    Player.List.ToList().ForEach(x => x.AddBroadcast(20, $"승리자 : {pl[0].Nickname}"));
+                    Player.List.ToList().ForEach(x => x.AddBroadcast(20, $"승리자 : {pl[0].DisplayNickname}"));
                 }
             }
         }
