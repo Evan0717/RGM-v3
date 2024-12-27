51c6277afbc7a8ce57a21af06e0af1eae63cac41
GoldenPig1205 <pkj7354@gmail.com>
2024-12-28 00:50:25 +0900

    소울메이트ㅣ무한 루프에 빠지지 않도록 코드 수정

--- RGM/RGM/Modes/Pluses/SoulMate/SoulMate.cs
+++ RGM/RGM/Modes/Pluses/SoulMate/SoulMate.cs
@@ -228,8 +228,11 @@ namespace RGM.Modes
 
                 if (soulMate != null && soulMate.IsAlive)
                 {
-                    soulMate.Kill($"{ev.Player.DisplayNickname}(와)과 {soulMate.DisplayNickname}(은)는 영혼의 단짝이였습니다.");
-                    ev.Player.Kill($"{soulMate.DisplayNickname}(와)과 {ev.Player.DisplayNickname}(은)는 영혼의 단짝이였습니다.");
+                    foreach (var player in Player.List.Where(x => x.IsDead))
+                        player.AddBroadcast(10, $"<size=20>{ev.Player.DisplayNickname}(와)과 {soulMate.DisplayNickname}(은)는 <color=#FE2EF7>소울메이트</color>였습니다.</size>");
+
+                    soulMate.Kill(ev.DamageHandler);
+
                     Server.ExecuteCommand($"/cassie_sl <color=red>{ev.Attacker.DisplayNickname}</color>(이)가 영혼의 단짝이였던 <color=#5858FA>{ev.Player.DisplayNickname}</color>와(과) <color=#FE2EF7>{soulMate.DisplayNickname}</color>을(를) 사이좋게 하늘로 보냈습니다.");
                 }
             }
