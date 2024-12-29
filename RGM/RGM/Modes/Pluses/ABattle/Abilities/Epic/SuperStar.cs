5ef139044a5104bec1c02e5799d2da46bf261f93
GoldenPig1205 <pkj7354@gmail.com>
2024-12-30 01:19:15 +0900

    슈퍼 스타ㅣ사망 시 사망 메세지가 모두에게 출력되지 않던 버그 수정

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Epic/SuperStar.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Epic/SuperStar.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using Exiled.API.Enums;
+using Exiled.API.Extensions;
 using Exiled.API.Features;
 using Exiled.API.Features.Items;
 using Exiled.Events.EventArgs.Player;
 using InventorySystem.Items.Usables.Scp330;
 using MEC;
+using MultiBroadcast.API;
+using RGM.API.DataBases;
 using RGM.API.Features;
 using UnityEngine;
 
@@ -18,10 +21,25 @@ public class SuperStar : Ability
     public override void OnEnabled()
     {
         Server.ExecuteCommand($"/speak {Owner.Id} 1");
+
+        Exiled.Events.Handlers.Player.Died += OnDied;
     }
 
     public override void OnDisabled()
     {
         Server.ExecuteCommand($"/speak {Owner.Id} 0");
+
+        Exiled.Events.Handlers.Player.Died -= OnDied;
+    }
+
+    public void OnDied(DiedEventArgs ev)
+    {
+        if (ev.Player != Owner)
+            return;
+
+        foreach (var player in Player.List)
+        {
+            player.AddBroadcast(10, $"<size=25><color=#FE2EF7>슈퍼 스타</color>였던 <color=#F4FA58>{Owner.DisplayNickname}</color>(<color={ev.TargetOldRole.GetColor().ToHex()}>{Trans.Role[ev.TargetOldRole]}</color>)(은)는 별세하셨습니다..</size>");
+        }
     }
 }
