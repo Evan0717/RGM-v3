1072c58240dd97d1acb3590c4efdf5d312100370
GoldenPig1205 <pkj7354@gmail.com>
2025-03-23 18:18:23 +0900

    맵 로드 시 로드된 맵 띄움

---
+++ RGM/RGM/EventArgs/MEREvents.cs
@@ -0,0 +1,39 @@
+﻿using MEC;
+using PlayerRoles;
+using RGM.API.Components;
+using RGM.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Exiled.API.Features;
+using MultiBroadcast.API;
+
+using static RGM.Variables.ServerManagers;
+
+using static RGM.Functions.ModeManagers;
+
+using static RGM.IEnumerators.LobbyManagers;
+using static RGM.IEnumerators.ServerManagers;
+
+using Exiled.API.Enums;
+using RGM.API.DataBases;
+using InventorySystem.Configs;
+
+namespace RGM.EventArgs
+{
+    public static class MEREvents
+    {
+        public static void OnLoadingMap(MapEditorReborn.Events.EventArgs.LoadingMapEventArgs ev)
+        {
+            Log.Info($"로드된 맵: {ev.NewMap.Name}");
+
+            foreach (var player in Player.List)
+            {
+                player.AddBroadcast(10, $"<size=20>맵 불러오기 완료: {ev.NewMap.Name}</size>");
+            }
+        }
+    }
+}
