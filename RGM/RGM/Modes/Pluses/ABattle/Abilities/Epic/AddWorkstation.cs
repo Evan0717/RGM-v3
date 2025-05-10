06a4d70ca74b3ccb74a6cdd3133d0992d64ed44f
GoldenPig1205 <pkj7354@gmail.com>
2025-05-10 21:43:04 +0900

    워업ㅣ"업무 증가" 능력 임시 삭제 조치

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Epic/AddWorkstation.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Epic/AddWorkstation.cs
@@ -14,19 +14,22 @@ using UnityEngine;
 
 namespace RGM.Modes.Abilities.Epic;
 
-[Ability("업무 증가", "새로운 워크스테이션을 발 아래에 생성합니다. 모두가 사용할 수 있습니다.", AbilityCategory.Epic, AbilityType.EPIC_ADDWORKSTATION)]
+//[Ability("업무 증가", "새로운 워크스테이션을 발 아래에 생성합니다. 모두가 사용할 수 있습니다. 길을 막으면 제재 대상입니다.", AbilityCategory.Epic, AbilityType.EPIC_ADDWORKSTATION)]
 public class AddWorkstation : Ability
 {
     public override void OnEnabled()
     {
-        ObjectSpawner.SpawnWorkstation(new WorkstationSerializable
+        if (Physics.Raycast(Owner.Position, Vector3.down, out RaycastHit hit, 100, (LayerMask)1))
         {
-            IsInteractable = true,
-            Position = new Vector3(Owner.Position.x, Owner.Position.y - 1.75f, Owner.Position.z),
-            Rotation = new Vector3(Owner.Rotation.x, Owner.Rotation.y, Owner.Rotation.z),
-            Scale = new Vector3(1, 1, 1),
-            RoomType = RoomType.Surface
-        });
+            ObjectSpawner.SpawnWorkstation(new WorkstationSerializable
+            {
+                IsInteractable = true,
+                Position = hit.point,
+                Rotation = new Vector3(90, Owner.Rotation.y, 0),
+                Scale = new Vector3(1, 1, 1),
+                RoomType = RoomType.Surface
+            });
+        }
     }
 
     public override void OnDisabled()
