71323fd533e72946431655e17b581c56bc156e26
박경현 <pkj7354@gmail.com>
2025-05-23 10:55:59 +0900

    오리지널ㅣ첫 릴리즈 (Set)

---
+++ RGM/RGM/Modes/Sets/Original/Original.cs
@@ -0,0 +1,50 @@
+﻿using AdminToys;
+using CommandSystem.Commands.RemoteAdmin;
+using CustomPlayerEffects;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.API.Features.Roles;
+using Exiled.Events.EventArgs.Scp244;
+using Exiled.Events.EventArgs.Server;
+using Interactables.Interobjects.DoorUtils;
+using MEC;
+using PlayerRoles;
+using PlayerStatsSystem;
+using Respawning;
+using RGM.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+using static RGM.Variables.ServerManagers;
+
+namespace RGM.Modes
+{
+    [Mode(ModeCategory.Public, ModeInfo.Set, ModeType.Original)]
+    public class Orignal : Mode
+    {
+        public override string Name => "오리지널";
+        public override string Description => "가끔은 도파민이 없는 기본을 해보시는 건 어떠신지..?";
+        public override string Detail =>
+"""
+모드가 존재하지 않습니다.
+""";
+        public override string Color => "FFFFFF";
+
+        public static Orignal Instance;
+
+        public override void OnEnabled()
+        {
+            Timing.RunCoroutine(OnModeStarted());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            yield break;
+        }
+    }
+}
