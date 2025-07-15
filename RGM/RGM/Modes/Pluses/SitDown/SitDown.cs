dfbe1f77cfda994f6ad55d8229297825c553b65f
박경현 <pkj7354@gmail.com>
2025-07-15 15:29:43 +0900

    뜀박질ㅣOnlySub 모드로 첫 출시

---
+++ RGM/RGM/Modes/Pluses/SitDown/SitDown.cs
@@ -0,0 +1,111 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Server;
+using MEC;
+using PlayerRoles;
+using PlayerStatsSystem;
+using RGM.API.DataBases;
+using RGM.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RGM.Modes
+{
+    [Mode(ModeCategory.OnlySub, ModeInfo.Plus, ModeType.SitDown)]
+    public class SitDown : Mode
+    {
+        public override string Name => "뜀박질";
+        public override string Description => "일어났다가 앉았다가";
+        public override string Detail =>
+"""
+인간은 ALT키를 통해 일어났다가 앉을 수 있습니다.
+""";
+        public override string Color => "d7dfbd";
+
+        public static SitDown Instance;
+
+        static Dictionary<Player, PlayerStatus> playerStatuses = new Dictionary<Player, PlayerStatus>();
+
+        class PlayerStatus
+        {
+            public bool IsSitDown { get; set; } = false;
+            public bool IsChangingSitDownState { get; set; } = false;
+        }
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.TogglingNoClip += OnTogglingNoClip;
+
+            Timing.RunCoroutine(OnModeStarted());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            yield break;
+        }
+
+        public static IEnumerator<float> OnTogglingNoClip(TogglingNoClipEventArgs ev)
+        {
+            if (!playerStatuses[ev.Player].IsChangingSitDownState && !ev.Player.IsJumping && !ev.Player.IsNoclipPermitted && ev.Player.IsHuman)
+            {
+                playerStatuses[ev.Player].IsChangingSitDownState = true;
+
+                AudioPlayer audioPlayer = AudioPlayer.CreateOrGet($"Player {ev.Player.Nickname}", onIntialCreation: (p) =>
+                {
+                    Speaker speaker = p.AddSpeaker("Main", maxDistance: 3);
+
+                    p.transform.parent = ev.Player.GameObject.transform;
+
+                    speaker.transform.parent = ev.Player.GameObject.transform;
+
+                    speaker.transform.localPosition = Vector3.zero;
+                });
+
+                if (playerStatuses[ev.Player].IsSitDown)
+                {
+                    audioPlayer.TryPlay($"standing");
+
+                    while (ev.Player.Scale.y >= 0.65f)
+                    {
+                        ev.Player.Scale = new Vector3(1, ev.Player.Scale.y - 0.01f, 1);
+
+                        yield return Timing.WaitForOneFrame;
+                    }
+
+                    ev.Player.EnableEffect(EffectType.Slowness, 50);
+                    ev.Player.EnableEffect(EffectType.SilentWalk, 10);
+
+                    playerStatuses[ev.Player].IsSitDown = false;
+                }
+                else
+                {
+                    if (!Physics.Raycast(ev.Player.Position, Vector3.up, out RaycastHit hit, 1, (LayerMask)1))
+                    {
+                        audioPlayer.TryPlay($"sitting");
+
+                        while (ev.Player.Scale.y <= 1)
+                        {
+                            ev.Player.Scale = new Vector3(1, ev.Player.Scale.y + 0.01f, 1);
+
+                            yield return Timing.WaitForOneFrame;
+                        }
+
+                        ev.Player.DisableEffect(EffectType.Slowness);
+                        ev.Player.DisableEffect(EffectType.SilentWalk);
+
+                        playerStatuses[ev.Player].IsSitDown = true;
+                    }
+                }
+
+                playerStatuses[ev.Player].IsChangingSitDownState = false;
+            }
+        }
+    }
+}
