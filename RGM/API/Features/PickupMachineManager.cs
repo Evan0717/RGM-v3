0e477ca167948f590f5e3949d62d3b48e7943f76
GoldenPig1205 <pkj7354@gmail.com>
2025-07-25 19:06:53 +0900

    인형 뽑기를 만드려고 시도함

---
+++ RGM/API/Features/PickupMachineManager.cs
@@ -0,0 +1,108 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using MEC;
+using ProjectMER.Features;
+using ProjectMER.Features.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static PlayerRoles.PlayableScps.Scp939.Mimicry.EnvMimicrySequence;
+
+namespace RGM.API.Features
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class SetPickupMachine : ICommand
+    {
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            PickupMachineManager.OnEnabled();
+
+            response = $"뽑기 기계를 추가하는데 성공했습니다.";
+            return true;
+        }
+
+        public string Command { get; } = "setpm";
+
+        public string[] Aliases { get; } = { "spm" };
+
+        public string Description { get; } = "뽑기 기계ㅣ뽑기 기계를 추가합니다.";
+    }
+
+    public static class PickupMachineManager
+    {
+        public static void OnEnabled()
+        {
+            Create(new Vector3(40f, 314.08f, -32.6f));
+        }
+
+        static void Create(Vector3 pos)
+        {
+            SchematicObject pickupMachine = ObjectSpawner.SpawnSchematic("DollGrabber", pos);
+            GameObject grabber = pickupMachine.AttachedBlocks.First(x => x.name == "Grabber");
+            int chance = 0;
+            int remain = 0;
+            bool isRunning = false;
+
+            IEnumerator<float> onCoinInserted()
+            {
+                while (true)
+                {
+                    if (isRunning)
+                    {
+                        remain -= 1;
+
+                        if (remain <= 0)
+                        {
+                            isRunning = false;
+
+                            yield return Timing.WaitForSeconds(5);
+                        }
+                    }
+
+                    if (chance > 0 && !isRunning)
+                    {
+                        chance -= 1;
+                        isRunning = true;
+                        remain = 20;
+                    }
+
+                    yield return Timing.WaitForSeconds(1);
+                }
+            }
+
+            void OnSearchingPickup(SearchingPickupEventArgs ev)
+            {
+                if (ev.Pickup.Transform.name == "←")
+                {
+
+                }
+
+                if (ev.Pickup.Transform.name.StartsWith("DollGrabberInput"))
+                {
+                    if (ev.Player.CurrentItem != null && ev.Player.CurrentItem.Type == ItemType.Coin)
+                    {
+                        ev.IsAllowed = false;
+
+                        ev.Player.RemoveItem(ev.Player.CurrentItem);
+
+                        Tools.PlaySound(ev.Pickup.Transform, "vm_insert_success", 2);
+
+                        chance += 1;
+                    }
+                    else
+                    {
+                        Tools.PlaySound(ev.Pickup.Transform, "vm_insert_fail", 2);
+                    }
+                }
+            }
+
+            Timing.RunCoroutine(onCoinInserted());
+
+            Exiled.Events.Handlers.Player.SearchingPickup += OnSearchingPickup;
+        }
+    }
+}
