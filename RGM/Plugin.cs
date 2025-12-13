5b2d922a2086bd477f1211ef2aa401b3deab0f91
GoldenPig1205 <pkj7354@gmail.com>
2025-12-13 15:05:43 +0900

    랜덤게임모드> v3.19.12 릴리즈

--- RGM/Plugin.cs
+++ RGM/Plugin.cs
@@ -36,7 +36,7 @@ namespace RGM
 
         public override string Name => "RGM";
         public override string Author => "GoldenPig1205";
-        public override Version Version { get; } = new(3, 19, 11);
+        public override Version Version { get; } = new(3, 19, 12);
         public override Version RequiredExiledVersion { get; } = new(1, 2, 0, 5);
 
         public override void OnEnabled()
@@ -99,7 +99,6 @@ namespace RGM
             Exiled.Events.Handlers.Player.DroppingAmmo += OnDroppingAmmo;
             Exiled.Events.Handlers.Player.DroppedItem += OnDroppedItem;
             Exiled.Events.Handlers.Player.DroppedAmmo += OnDroppedAmmo;
-            Exiled.Events.Handlers.Player.ItemAdded += OnItemAdded;
             Exiled.Events.Handlers.Player.Shooting += OnShooting;
             Exiled.Events.Handlers.Player.Kicking += OnKicking;
             Exiled.Events.Handlers.Player.Banning += OnBanning;
