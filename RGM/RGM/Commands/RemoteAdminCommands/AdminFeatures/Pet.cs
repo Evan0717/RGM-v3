e103344c05ffd8a16fd45c741fc203e61d871ce5
GoldenPig1205 <pkj7354@gmail.com>
2025-12-24 12:17:51 +0900

    0.5% 확률로 해당 라운드에서 카피바라가 당신을 따라다닙니다.

---
+++ RGM/RGM/Commands/RemoteAdminCommands/AdminFeatures/Pet.cs
@@ -0,0 +1,46 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using Exiled.API.Features.Pickups;
+using Exiled.API.Features.Toys;
+using RGM.Modes.SubClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RGM.Commands.RemoteAdminCommands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class Pet : ICommand
+    {
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get(arguments.At(0));
+
+            if (CapybaraPet.Players.Contains(player))
+            {
+                CapybaraPet.Players.Remove(player);
+
+                response = "Pet has been disabled.";
+            }
+            else
+            {
+                CapybaraPet.Create(player);
+
+                response = "Pet has been enabled.";
+            }
+
+            return true;
+        }
+
+        public string Command { get; } = "pet";
+
+        public string[] Aliases { get; } = { "펫" };
+
+        public string Description { get; } = "펫을 켜거나 끕니다.";
+
+        public bool SanitizeResponse { get; } = true;
+    }
+}
