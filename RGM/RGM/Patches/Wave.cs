800a838ff60997ce86e95550ca080fee93e5a9cc
GoldenPig1205 <pkj7354@gmail.com>
2026-01-03 13:20:08 +0900

    지원 관련 하모니 패치가 서버를 터트리는 것 같아서 안정화

--- RGM/RGM/Patches/Wave.cs
+++ RGM/RGM/Patches/Wave.cs
@@ -2,6 +2,7 @@
 using MultiBroadcast.API;
 using RGM.API.Features;
 using RGM.Modes.SubClass;
+using System;
 using static RGM.Variables.Variable;
 
 namespace RGM.Patches
@@ -10,15 +11,26 @@ namespace RGM.Patches
     {
         public static void Postfix(ReferenceHub player, ref bool __result)
         {
-            if (player.IsHost || player.IsDummy)
+            try
             {
-                __result = false;
-            }
-            else
-            {
-                Player ply = Player.Get(player);
+                if (!__result)
+                    return;
 
-                __result = UsersManager.UsersCache[ply.UserId][23] == "0" && (ply.IsDead || NonePlayer.Players.Contains(ply));
+                if (player.IsHost || player.IsDummy)
+                {
+                    __result = false;
+                }
+                else
+                {
+                    Player ply = Player.Get(player);
+
+                    __result = !ply.IsDND() && (ply.IsDead || ply.IsNonePlayer());
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error($"[WavePostfix] Exception: {e}");
+                __result = false;
             }
         }
     }
