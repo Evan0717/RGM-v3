3dd09f1f394e825c623ecbc96764a972a7f140ba
babycatoffical <arazzang880331@gmail.com>
2026-04-12 00:11:04 +0900

    fix. /speak 남발 방지를 위하여 관련 조건문 추가

--- RGM/RGM/Modes/Plus/SuperStar/SuperStar.cs
+++ RGM/RGM/Modes/Plus/SuperStar/SuperStar.cs
@@ -13,9 +13,7 @@ namespace RGM.Modes
         public override string Name => "슈퍼 스타";
         public override string Description => "모두의 마이크가 공유됩니다.";
         public override string Detail =>
-"""
-말 그대로입니다. 마이크가 공유됩니다.
-""";
+"말 그대로입니다. 마이크가 공유됩니다.";
         public override string Color => "FE2EF7";
 
         public static SuperStar Instance;
@@ -34,15 +32,26 @@ namespace RGM.Modes
 
         public IEnumerator<float> OnModeStarted()
         {
+            byte count = 0;
             while (true)
             {
-                Server.ExecuteCommand($"/speak {string.Join(".", PlayerManager.List.Select(x => x.Id))}. 1");
-                foreach (var player in PlayerManager.List)
+                if (!IntercomPlayers.Exists(player => PlayerManager.List.Contains(player)))
                 {
-                    if (!IntercomPlayers.Contains(player))
+                    Server.ExecuteCommand($"/speak {string.Join(".", PlayerManager.List.Select(x => x.Id))}. 1");
+                    foreach (var player in PlayerManager.List.Where(player => !IntercomPlayers.Contains(player)))
+                    {
                         IntercomPlayers.Add(player);
+                    }
                 }
-
+                
+                if (count >= 120)
+                {
+                    IntercomPlayers.Clear();
+                    Log.Info("[RGM:SuperStar] 2분이 되어 자동으로 IntercomPlayers를 초기화합니다. ");
+                    count = 0;
+                }
+                
+                count += 1;
                 yield return Timing.WaitForSeconds(1f);
             }
         }
