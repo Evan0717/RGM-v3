a999664e0a984cdc4f44bec038ea36ff20598806
babycatoffical <wjsgusals32@gmail.com>
2026-04-26 00:01:28 +0900

    feat. 제거, 초기화, 사용 방법 기능 추가
    Signed-off-by: babycatoffical <wjsgusals32@gmail.com>

--- RGM/RGM/Modes/Plus/KoreanSpeed/Commands/RACommands/SetCount.cs
+++ RGM/RGM/Modes/Plus/KoreanSpeed/Commands/RACommands/SetCount.cs
@@ -18,7 +18,7 @@ public class SetCount : ICommand
                 return false;
             }
 
-            if (!SpeedStore.isEnabled)
+            if (!SpeedStore.IsEnabled)
             {
                 response = "해당 모드가 활성화되지 않았습니다. 활성화된 상태에서 다시 시도하세요.";
                 return false;
@@ -29,52 +29,72 @@ public class SetCount : ICommand
                 response = SpeedStore.Count <= 0 ? "그 누구도 죽지 않았습니다." :
                     SpeedStore.Count >= 125 ? "이 시체더미의 제단 속에서 당신의 속도는 한계까지 빨라집니다." :
                     $"{SpeedStore.Count}번의 죽음만큼 속도가 빨라집니다.";
+                KoreanSpeed.AddEffects();
                 return true;
             }
 
-            if (Regex.IsMatch(arguments.At(0), @"^[A-Za-z]+$") || arguments.At(0).Contains(""))
+            if (arguments.At(0).Equals("clear", StringComparison.OrdinalIgnoreCase))
             {
-                response = "알 수 없는 조건입니다. /setcount help으로 사용방법을 확인하세요.";
+                SpeedStore.Clear();
+                KoreanSpeed.AddEffects();
+                response = "해당 명령이 성공적으로 처리되었습니다.";
+                return true;
+            }
+
+            if (arguments.At(0).Equals("help", StringComparison.OrdinalIgnoreCase))
+            {
+                response = """ 
+                           /setcount 명령어 by babycat_official
+                           add <숫자> - 현재 횟수에 숫자를 추가합니다.
+                           set <숫자> - 현재 횟수를 숫자로 설정합니다.
+                           remove (또는 rm) <숫자> - 현재 횟수에서 숫자를 제거합니다.
+                           clear - 현재 횟수를 0으로 설정합니다.
+                           help - 도움말을 표시합니다.
+                           """;
                 return false;
             }
-            
-            if (byte.TryParse(arguments.At(1), out var data))
+
+            if (arguments.Count < 2)
             {
-                if (data <= 0)
-                {
-                    response = "최소한, 0 이상의 값을 입력하세요.";
-                    return false;
-                }
-                
+                response = "숫자를 입력해주세요.";
+                return false;
+            }
+            var data = byte.TryParse(arguments.At(1), out var value);
+            
+            if (Regex.IsMatch(arguments.At(1), @"^[0-9]+$") && data && value <= 125)
+            {
                 switch (arguments.At(0))
                 {
                     case "add":
-                        SpeedStore.Count += data + SpeedStore.Count > 125 ? (byte)125 : data;
+                        SpeedStore.Count += value + SpeedStore.Count > 125 ? (byte)125 : value;
                         break;
                     case "set":
-                        SpeedStore.Count = data + SpeedStore.Count > 125 ? (byte)125 : data;
+                        SpeedStore.Count = value + SpeedStore.Count > 125 ? (byte)125 : value;
                         break;
                     case "remove":
                     case "rm":
-                        SpeedStore.Count -= (int)SpeedStore.Count - data < 0 ? (byte)125 : (byte)(SpeedStore.Count - data);
-                        break;
-                    case "clear":
-                        SpeedStore.Clear();
-                        break;
+                        return SpeedStore.TryRemove(value, out response);
                     default:
-                        response = "알 수 없는 조건입니다.";
+                        response = """ 
+                                   알 수 없는 명령어입니다.
+                                   add <숫자> - 현재 횟수에 숫자를 추가합니다.
+                                   set <숫자> - 현재 횟수를 숫자로 설정합니다.
+                                   remove (또는 rm) <숫자> - 현재 횟수에서 숫자를 제거합니다.
+                                   clear - 현재 횟수를 0으로 설정합니다.
+                                   help - 도움말을 표시합니다.
+                                   """;
                         return false;
                 }
+                KoreanSpeed.AddEffects();
             }
             else
             {
                 response = """
                            수가 알맞지 않거나, 너무 큽니다.
-                           0 ~ 255 사이의 수이면서 문자가 들어가지 않은 수를 입력해주세요.
+                           0 ~ 125 사이의 수이면서 문자가 들어가지 않은 수를 입력해주세요.
                            """;
+                return false;
             }
-
-            KoreanSpeed.AddEffects();
 
             response = "해당 명령이 성공적으로 처리되었습니다.";
             return true;
@@ -100,6 +120,8 @@ public class SetCount : ICommand
         모드 "한국인이 좋아하는 속도"의 카운트를 설정합니다.
         추가: /setcount add <값>
         설정: /setcount set <값>
+        제거: /setcount remove <값> 또는 /setcount rm <값>
+        초기화: /setcount clear 
         (0 ~ 255 사이로 설정해주세요.)
         """;
 }
