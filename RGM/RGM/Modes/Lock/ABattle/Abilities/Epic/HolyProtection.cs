567baa950b81805b4bc74d779fb95dbb656fd271
Evan0711 <joungho0405@gmail.com>
2026-07-22 18:06:35 +0900

    디버프 제거 효과를 하나의 코드에서 처리

--- RGM/RGM/Modes/Lock/ABattle/Abilities/Epic/HolyProtection.cs
+++ RGM/RGM/Modes/Lock/ABattle/Abilities/Epic/HolyProtection.cs
@@ -1,40 +1,14 @@
-using System.Collections.Generic;
 using Exiled.API.Enums;
 using Exiled.API.Extensions;
 using Exiled.Events.EventArgs.Player;
 using MEC;
+using RGM.API.Features;
 
 namespace RGM.Modes.Abilities.Epic;
 
 [Ability("신성방어", "모든 디버프에 면역을 가집니다.", AbilityCategory.Epic, AbilityType.EPIC_HOLYPROTECTION)]
 public class HolyProtection : Ability
 {
-    private static readonly HashSet<EffectType> KeptBuffs =
-    [
-        EffectType.Scp1853,
-        EffectType.Invigorated,
-        EffectType.Invisible,
-        EffectType.RainbowTaste,
-        EffectType.BodyshotReduction,
-        EffectType.DamageReduction,
-        EffectType.MovementBoost,
-        EffectType.Vitality,
-        EffectType.SpawnProtected,
-        EffectType.Ghostly,
-        EffectType.SilentWalk,
-        EffectType.Fade,
-        EffectType.FocusedVision,
-        EffectType.AnomalousRegeneration,
-        EffectType.Scp1344,
-        EffectType.Scp207,
-        EffectType.AntiScp207,
-        EffectType.Lightweight,
-        EffectType.NightVision,
-        EffectType.FogControl,
-        EffectType.PitDeath,
-        EffectType.Decontaminating
-    ];
-
     public override void OnEnabled()
     {
         Exiled.Events.Handlers.Player.ReceivingEffect += OnReceivingEffect;
@@ -50,7 +24,7 @@ public class HolyProtection : Ability
 
         var effectType = ev.Effect.GetEffectType();
 
-        if (!KeptBuffs.Contains(effectType))
+        if (!EffectManager.IsKeptBuff(effectType))
         {
             Timing.CallDelayed(Timing.WaitForOneFrame, () =>
             {
