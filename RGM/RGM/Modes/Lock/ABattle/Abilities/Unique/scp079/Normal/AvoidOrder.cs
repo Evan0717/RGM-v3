142a7034e6fb4608c1b47d2943b38338eb088fe2
FlyingRagdoll <FlyingRagd01l01@gmail.com>
2026-07-26 16:33:57 +0900

    워크 밸패

--- RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Normal/AvoidOrder.cs
+++ RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Normal/AvoidOrder.cs
@@ -9,16 +9,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RGM.Modes.Abilities.Unique.Scp079.Rare;
+namespace RGM.Modes.Abilities.Unique.Scp079.Common;
 
-[Ability("회피 명령", "SCP들에게 [일반] 민첩 능력을 2개 지급합니다.", AbilityCategory.Rare, AbilityType.RARE_SCP079_AVOIDORDER, RoleAbility.Scp079)]
+[Ability("회피 명령", "SCP들에게 [일반] 민첩 능력을 3개 지급합니다.", AbilityCategory.Common, AbilityType.NORMAL_SCP079_AVOIDORDER, RoleAbility.Scp079)]
 public class AvoidOrder : Ability
 {
     public override void OnEnabled()
     {
         foreach (var scp in PlayerManager.List.Where(x => x.IsScpRole() && x.Role.Type != RoleTypeId.Scp079))
         {
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 3; i++)
             {
                 scp.AddAbility(AbilityType.NORMAL_AGILITY);
             }
