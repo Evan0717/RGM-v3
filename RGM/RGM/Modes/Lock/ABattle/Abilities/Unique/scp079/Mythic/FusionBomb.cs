1b0f89740d085ccad4f217ea1b43d127820d32d3
FlyingRagdoll <FlyingRagd01l01@gmail.com>
2026-07-26 17:00:11 +0900

    Update FusionBomb.cs

--- RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Mythic/FusionBomb.cs
+++ RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Mythic/FusionBomb.cs
@@ -11,7 +11,7 @@ using ProjectMER.Features;
 namespace RGM.Modes.Abilities.Unique.Scp079.Mythic;
 
 
-[Ability("융단 폭격", "핑을 찍은 지점 20m 이내에서 10초간 0.25초 간격으로 미사일이 쏟아집니다. (쿨타임 15초)", AbilityCategory.Mythic, AbilityType.MYTHIC_SCP079_FUSIONBOMB, RoleAbility.Scp079)]
+[Ability("융단 폭격", "핑을 찍은 지점 20m 이내에서 10초간 0.1초 간격으로 미사일이 쏟아집니다. (쿨타임 15초)", AbilityCategory.Mythic, AbilityType.MYTHIC_SCP079_FUSIONBOMB, RoleAbility.Scp079)]
 public class FusionBomb : Ability
 {
     bool isScp079Cooldown = false;
@@ -57,7 +57,7 @@ public class FusionBomb : Ability
     public IEnumerator<float> StartBombardment(Vector3 centerPos)
     {
         float duration = 10f;
-        float interval = 0.25f;
+        float interval = 0.1f;
         float elapsedTime = 0f;
 
         while (elapsedTime < duration)
