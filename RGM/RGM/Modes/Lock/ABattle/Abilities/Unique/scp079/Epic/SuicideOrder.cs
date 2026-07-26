88fb90b425b165fad8bff382c637d975b370359f
FlyingRagdoll <FlyingRagd01l01@gmail.com>
2026-07-26 17:05:39 +0900

    .

--- RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Epic/SuicideOrder.cs
+++ RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Epic/SuicideOrder.cs
@@ -12,7 +12,7 @@ using Random = UnityEngine.Random;
 
 namespace RGM.Modes.Abilities.Unique.Scp079.Epic;
 
-[Ability("희생 명령", "SCP들에게 [<color=#FF00FF>영웅</color>]테러리스트의 유품 능력을 4개 지급합니다.", AbilityCategory.Epic, AbilityType.EPIC_SCP079_SUICIDEORDER, RoleAbility.Scp079)]
+//[Ability("희생 명령", "SCP들에게 [<color=#FF00FF>영웅</color>]테러리스트의 유품 능력을 4개 지급합니다.", AbilityCategory.Epic, AbilityType.EPIC_SCP079_SUICIDEORDER, RoleAbility.Scp079)]
 public class SuicideOrder : Ability
 {
     public override void OnEnabled()
--- RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Epic/SurvivalOrder.cs
+++ RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Epic/SurvivalOrder.cs
@@ -12,14 +12,14 @@ using Random = UnityEngine.Random;
 
 namespace RGM.Modes.Abilities.Unique.Scp079.Epic;
 
-[Ability("생존 명령", "SCP들에게 [<color=#FF00FF>영웅</color>]구사일생 능력 5개를 지급합니다.", AbilityCategory.Epic, AbilityType.EPIC_SCP079_SURVIVALORDER, RoleAbility.Scp079)]
+[Ability("생존 명령", "SCP들에게 [<color=#FF00FF>영웅</color>]구사일생 능력 2개를 지급합니다.", AbilityCategory.Epic, AbilityType.EPIC_SCP079_SURVIVALORDER, RoleAbility.Scp079)]
 public class SurvivalOrder : Ability
 {
     public override void OnEnabled()
     {
         foreach (var scp in PlayerManager.List.Where(x => x.IsScpRole() && x.Role.Type != RoleTypeId.Scp079))
         {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 2; i++)
             {
                 scp.AddAbility(AbilityType.EPIC_SURVIVOR);
             }
