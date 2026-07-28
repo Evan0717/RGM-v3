f5e788538bc09a67f88c827a7be8b5d2c07db9ee
FlyingRagdoll <FlyingRagd01l01@gmail.com>
2026-07-28 21:05:50 +0900

    Update Protection.cs

--- RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Rare/Protection.cs
+++ RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Rare/Protection.cs
@@ -11,14 +11,14 @@ using System.Threading.Tasks;
 
 namespace RGM.Modes.Abilities.Unique.Scp079.Rare;
 
-[Ability("보호", "SCP들에게 [<color=#2ECCFA>희귀</color>] 하이패스, [<color=#2ECCFA>희귀</color>] 강첩껍질 능력 3개 지급합니다.", AbilityCategory.Rare, AbilityType.RARE_SCP079_PROTECTION, RoleAbility.Scp079)]
+[Ability("보호", "SCP들에게 [<color=#2ECCFA>희귀</color>] 하이패스, [<color=#2ECCFA>희귀</color>] 강첩껍질 능력 2개 지급합니다.", AbilityCategory.Rare, AbilityType.RARE_SCP079_PROTECTION, RoleAbility.Scp079)]
 public class Protection : Ability
 {
     public override void OnEnabled()
     {
         foreach (var scp in PlayerManager.List.Where(x => x.IsScpRole() && x.Role.Type != RoleTypeId.Scp079))
         {
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 2; i++)
             {
                 scp.AddAbility(AbilityType.RARE_STEELSHELL);
             }
