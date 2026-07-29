2cc67257bd03490a3c982f1b9f73b8aecc22ea35
babycatoffical <wjsgusals32@gmail.com>
2026-07-30 00:27:30 +0900

    fix. 정상화
    Signed-off-by: babycatoffical <wjsgusals32@gmail.com>

--- RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Rare/Repair.cs
+++ RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Rare/Repair.cs
@@ -1,29 +1,32 @@
 ﻿using Exiled.API.Features.Doors;
-using UnityEngine;
 using System.Collections.Generic;
+using Exiled.API.Features;
 using MEC;
 using Map = Exiled.API.Features.Map;
 
 namespace RGM.Modes.Abilities.Unique.Scp079.Rare;
 
-//[Ability("수리수리 마수리", "1 ~ 2분마다 부서진 모든 문이 각각 50% 확률로 복구됩니다.", AbilityCategory.Rare, AbilityType.RARE_SCP079_REPAIR, RoleAbility.Scp079)]
+// [Ability("수리수리 마수리", "1 ~ 2분마다 부서진 모든 문이 복구됩니다.", AbilityCategory.Rare, AbilityType.RARE_SCP079_REPAIR, RoleAbility.Scp079)]
 public class Repair : Ability
 {
     private CoroutineHandle _repairHandle;
     private static readonly System.Random Rand = new(Map.Seed);
     
-    public override void OnEnabled() 
-        => _repairHandle = Timing.RunCoroutine(RepairCoroutine());
+    public override void OnEnabled()
+    {
+        if (Timing.IsRunning(_repairHandle))
+            _repairHandle = Timing.RunCoroutine(RepairCoroutine());
+    }
 
     private static IEnumerator<float> RepairCoroutine()
     {
         while (true)
         {
+            Log.Info("Running");
             foreach (var door in Door.List)
             {
-                if (door is not BreakableDoor breakableDoor) continue;
-                    
-                if (Mathf.Clamp01((float)Rand.NextDouble()) <= .5f) { breakableDoor.Repair(); }
+                if (door is BreakableDoor breakableDoor) 
+                    breakableDoor.Repair();
             }
             Timing.WaitForSeconds(Rand.Next(1, 3) * 60);
         }
