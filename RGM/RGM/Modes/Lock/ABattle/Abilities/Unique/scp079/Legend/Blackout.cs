92aa85d0651b5432b93f3e9d576f600556828127
Evan0717 <joungho0405@gmail.com>
2026-07-29 14:15:38 +0900

    워크 | 1개 능력 추가 및 밸런스 패치

--- RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Legend/Blackout.cs
+++ RGM/RGM/Modes/Lock/ABattle/Abilities/Unique/scp079/Legend/Blackout.cs
@@ -12,7 +12,6 @@ using static RGM.Variables.Variable;
 
 namespace RGM.Modes.Abilities.Unique.Scp079.Legend;
 
-
 [Ability("블랙아웃", "시설 전체가 정전됩니다.", AbilityCategory.Legend, AbilityType.LEGEND_SCP079_BLACKOUT, RoleAbility.Scp079)]
 public class Blackout: Ability
 {
