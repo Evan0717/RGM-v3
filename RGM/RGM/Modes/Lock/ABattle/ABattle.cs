3daf42fa5d09efcae8bb7fa77b35f7bb5a8f3ee2
babycatoffical <wjsgusals32@gmail.com>
2026-08-11 12:06:33 +0900

    feat. 자동핵 기능이 제거된 OnEnabled() 메서드 추가 및 약간의 확률 조정
    Signed-off-by: babycatoffical <wjsgusals32@gmail.com>

--- RGM/RGM/Modes/Lock/ABattle/ABattle.cs
+++ RGM/RGM/Modes/Lock/ABattle/ABattle.cs
@@ -215,10 +215,7 @@ public class ABattle : Mode
 
         void ActivateExtraModeForChaos()
         {
-            
-            
             Timing.RunCoroutine(Chaos());
-            Tools.TryInstallMode(ModeType.FriendlyFire);
             return;
 
             IEnumerator<float> Chaos()
@@ -227,12 +224,15 @@ public class ABattle : Mode
                 const float chaosStopTime = 30f;
                 while (EnabledModeList.Exists(x => x.Data.Type == ModeType.ABattle))
                 {
+                    FriendlyFire.Instance.OnEnabledForNoNuke();
                     Tools.MessageTranslated(".G6 .G6 .G6",
                         "10초 후 무언가가 일어납니다.");
                     if (!EnabledModeList.Exists(x => x.Data.Type == ModeType.ABattle)) break;
                     Timing.CallDelayed(waitTime, () => Timing.RunCoroutine(ChaosMaker()));
                     yield return Timing.WaitForSeconds(chaosStopTime + waitTime);
                 }
+
+                FriendlyFire.Instance.OnDisabled();
             }
 
             IEnumerator<float> ChaosMaker()
@@ -241,7 +241,7 @@ public class ABattle : Mode
                 const int mythicChance = 97;
                 const int legendaryChance = 95;
                 const int epicChance = 75;
-                const int explodeChance = 80;
+                const int explodeChance = 60;
                 const int explodeCount = 5;
                 
                 if (!_chaosMutex.WaitOne(1000)) yield break;
