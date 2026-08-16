5b63fe92e1b1df3bc46385933e5493f1ac138382
Evan0717 <joungho0405@gmail.com>
2026-08-16 21:18:29 +0900

    전략적 팀 전투 | 버그 수정

--- RGM/RGM/Modes/Lock/TFT/TFT/TFTAbilities/Safe/Human/Kick.cs
+++ RGM/RGM/Modes/Lock/TFT/TFT/TFTAbilities/Safe/Human/Kick.cs
@@ -9,7 +9,8 @@ namespace DAONTFT.Core.TFT.Safe.Human;
 [TFTAbility("태권도", "능력 키(ALT)를 눌러 발차기를 할 수 있습니다. 부위별로 데미지가 다르게 적용됩니다.", TFTAbilityLevel.Safe, TFTAbilityCategory.Human, TFTAbilityPoint.ALT, TFTAbilityType.Kick, "💫")]
 public class Kick : TFTAbility
 {
-    public static int MeleeCooldown = 0;
+    private const float MeleeDamage = 50f;
+    private static int MeleeCooldown;
 
     public override void OnEnabled()
     {
@@ -21,43 +22,42 @@ public class Kick : TFTAbility
         Exiled.Events.Handlers.Player.TogglingNoClip -= OnTogglingNoClip;
     }
 
-    public void OnTogglingNoClip(TogglingNoClipEventArgs ev)
+    private void OnTogglingNoClip(TogglingNoClipEventArgs ev)
     {
         if (ev.Player != Owner)
             return;
 
-        if (Tools.TryGetLookPlayer(ev.Player, 3f, out Player player, out RaycastHit? hit))
+        if (!ev.Player.TryGetLookPlayer(4.5f, out Player player, out RaycastHit? hit)) return;
+        if (ev.Player == player || MeleeCooldown > 0 ||
+            !HitboxIdentity.IsEnemy(ev.Player.ReferenceHub, player.ReferenceHub)) return;
+
+        float damage = DmgCalc(hit?.transform.name);
+
+        if (player.IsScp)
         {
-            if (ev.Player != player && MeleeCooldown <= 0 && HitboxIdentity.IsEnemy(ev.Player.ReferenceHub, player.ReferenceHub))
+            damage *= 4;
+        }
+
+        Hitmarker.SendHitmarkerDirectly(ev.Player.ReferenceHub, damage / MeleeDamage);
+        player.Hit(ev.Player, damage);
+
+        MeleeCooldown = 1;
+
+        Timing.CallDelayed(1f, () => MeleeCooldown = 0);
+        return;
+
+        float DmgCalc(string pos)
+        {
+            switch (pos)
             {
-                float damageCalcu(string pos)
-                {
-                    switch (pos)
-                    {
-                        case "Head":
-                            return 40f;
+                case "Head":
+                    return MeleeDamage * 2f;
 
-                        case "Chest":
-                            return 20f;
+                case "Chest":
+                    return MeleeDamage;
 
-                        default:
-                            return 14f;
-                    }
-                }
-
-                float damage = damageCalcu(hit.Value.transform.name);
-
-                if (player.IsScp)
-                {
-                    damage *= 2;
-                }
-
-                Hitmarker.SendHitmarkerDirectly(ev.Player.ReferenceHub, damage / 20);
-                player.Hit(ev.Player, damage);
-
-                MeleeCooldown = 1;
-
-                Timing.CallDelayed(1f, () => MeleeCooldown = 0);
+                default:
+                    return MeleeDamage * 0.7f;
             }
         }
     }
