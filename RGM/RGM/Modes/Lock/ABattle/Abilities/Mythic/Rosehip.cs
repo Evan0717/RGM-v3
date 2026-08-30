7ee043607fbfbd38648a5191b5544d83452564a4
Evan0717 <joungho0405@gmail.com>
2026-08-30 17:38:57 +0900

    워크 | 버그 수정 및 능력 1개 조정

--- RGM/RGM/Modes/Lock/ABattle/Abilities/Mythic/Rosehip.cs
+++ RGM/RGM/Modes/Lock/ABattle/Abilities/Mythic/Rosehip.cs
@@ -1,4 +1,5 @@
-﻿using Exiled.API.Extensions;
+﻿using Exiled.API.Enums;
+using Exiled.API.Extensions;
 using Exiled.API.Features.Items;
 using Exiled.Events.EventArgs.Player;
 using PlayerRoles;
@@ -8,18 +9,16 @@ namespace RGM.Modes.Abilities.Mythic;
 
 [Ability("장미칼", """
                 이 명검은 무한으로 발산하는 힘을 가지고 있습니다...
-                35% 확률로 진영을 변경하며, 변경 실패 시 마다 공격력이 2배 증가합니다. 기본 데미지가 50 증가합니다.
+                50% 확률로 진영을 변경하며, 변경 실패 시 적을 『사망』 시킵니다.
                 """, AbilityCategory.Mythic, AbilityType.MYTHIC_ROSEHIP)]
 public class Rosehip : Ability
 {
     private ushort _serial;
-    private float _damageMultiplier = 2f;
 
     public override void OnEnabled()
     {
         Item item = Owner.AddItem(ItemType.SCP1509);
         _serial = item.Serial;
-        _damageMultiplier = 2f;
 
         Exiled.Events.Handlers.Player.ChangedItem += OnChangedItem;
         Exiled.Events.Handlers.Player.Hurting += OnHurting;
@@ -38,15 +37,13 @@ public class Rosehip : Ability
         if (ev.Attacker == null ||
             ev.Attacker.CurrentItem == null ||
             ev.Attacker.CurrentItem.Serial != _serial) return;
-        if (UnityEngine.Random.Range(1, 101) <= 35)
+        if (UnityEngine.Random.Range(1, 101) <= 50)
         {
-            _damageMultiplier = 2f;
             ev.IsAllowed = false;
             ev.Player.Role.Set(Tools.EnumToList<RoleTypeId>().GetRandomValue(x => x.GetSide() == ev.Attacker.Role.Type.GetSide()), RoleSpawnFlags.None);
             return;
         }
-
-        ev.DamageHandler.Damage *= _damageMultiplier;
-        _damageMultiplier *= 2f;
+        
+        ev.Player.Hurt(amount: ev.Player.MaxHealth, damageType: DamageType.Crushed, attacker: ev.Attacker);
     }
 }
