a1725396dc156514b0227631a7f10ddc11d24c58
FlyingRagdoll <FlyingRagd01l01@gmail.com>
2026-09-13 20:09:24 +0900

    Update Stroking999.cs

--- RGM/RGM/Modes/Set/Stroking999/Stroking999.cs
+++ RGM/RGM/Modes/Set/Stroking999/Stroking999.cs
@@ -29,7 +29,7 @@ namespace RGM.Modes
         private int TimeLimit;
         private const string TargetObjectName = "Scp999"; // 목표 오브젝트 이름
 
-        private static readonly int RaycastMask = 0;
+        private static readonly int RaycastMask = 1 << 0;
         private readonly Dictionary<Player, int> _scores = new();
         private bool gameEnded = false;
 
